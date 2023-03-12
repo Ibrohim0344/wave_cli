4c80b8a0b443f2c7d1a29bb8534a04f9493ad2f9
Ibrohim0344 <110087575+Ibrohim0344@users.noreply.github.com>
2023-03-12 15:48:16 +0500

    comment

--- bin/wave_cli.dart
+++ bin/wave_cli.dart
@@ -3,3 +3,10 @@ import 'package:wave_sdk/wave_sdk.dart';
 void main(List<String> arguments) {
   print(Calculator.summ([34, 67, 354, 6799, 3463]));
 }
+/*
+Created by Ibrohimjon Odilov on 15:46:54 12.03.2023
+© 2023 @Lord0344 
+*/
+/*
+*Theme:
+*/
